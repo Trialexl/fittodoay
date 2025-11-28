8f1fdc2a2eda93b9193d39d8d14b14f2b093e00d
Trialexl <86865993+Trialexl@users.noreply.github.com>
2025-11-28 21:07:18 +0300

    удалил мобильную разработку. начинаем заново в новой ветке

--- mobile/src/config.ts
+++
@@ -1,6 +0,0 @@
-import Constants from "expo-constants";
-
-const fallbackHost = "http://localhost:8000";
-const apiHost = (Constants.expoConfig?.extra as { apiBaseUrl?: string } | undefined)?.apiBaseUrl || fallbackHost;
-
-export const API_BASE_URL = `${apiHost.replace(/\\/$/, "")}/api`;
